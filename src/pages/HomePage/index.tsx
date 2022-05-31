7780f65f118c4ddd0cd95c5c7592e295393914d2
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-31 08:36:42 -0300

    styling input and table

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,3 +1,18 @@
+import styles from './styles.module.scss';
+
+import { BillingFilter } from '../../BillingFilter';
+import { Search } from '../../components/Search';
+import { StoreTable } from '../../components/StoreTable';
+
 export function HomePage() {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.storesContainer}>
+        <Search />
+        <StoreTable />
+      </div>
+
+      <BillingFilter />
+    </div>
+  );
 }
